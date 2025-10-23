7aa129966ec241504c340ff8f39606046a971659
IgorNiemiec <igorni3miec@gmail.com>
2025-10-23 13:21:59 +0200

    Project Setup
    All project files and dependencies have been initialized

---
+++ src/main/java/org/example/medical_clinic_management_system/MedicalClinicManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package org.example.medical_clinic_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalClinicManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalClinicManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/medical_clinic_management_system/MedicalClinicManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.medical_clinic_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalClinicManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
